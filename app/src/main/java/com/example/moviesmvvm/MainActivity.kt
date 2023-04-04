83c4ecc208068a897e52004859ddc64333633e29
AmazingHorsess <amazinghorsess@gmail.com>
2023-04-04 18:38:48 +0600

    Retrofit, network setup

--- app/src/main/java/com/example/moviesmvvm/MainActivity.kt
+++ app/src/main/java/com/example/moviesmvvm/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.moviesmvvm.navigation.MoviesNavHost
 import com.example.moviesmvvm.ui.theme.MoviesMVVMTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
